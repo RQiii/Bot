12b2aec8df9fdd9e068ae58f1f779551c14c16d4
RQiii <103615247+RQiii@users.noreply.github.com>
2022-04-14 19:54:03 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -5,7 +5,7 @@ from userbot import StartTime, iqthon
 from userbot.Config import Config
 from userbot.plugins import mention
 help1 = ("**🇮🇶⦙ كيفيه التنصيب :**")
-help2 = ("**🇮🇶⦙ قـائمـه الاوامـر :**\n**🇮🇶⦙ قنـاه السـورس :** @pmmvn** ")
+help2 = ("**🇮🇶⦙ قـائمـه الاوامـر :**\n**🇮🇶⦙ قنـاه السـورس :** @pmmvn ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
 Sour = f"**.• : version 7.5  °.** \n.• : me  {mention}  °. \n**.• : time  {TM}  °.**\n**.• : My Bot {TG_BOT} °.**\n**.• : Source : @pmmvn  °.**"
