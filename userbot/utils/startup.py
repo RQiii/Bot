5c2e2e6afe862bb1a8281442efc6968e5c311668
- Haidar <102754927+aaUUa@users.noreply.github.com>
2022-04-15 00:04:44 +0300

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -13,7 +13,7 @@ from ..core.session import iqthon
 from ..helpers.utils import install_pip
 from ..sql_helper.global_collection import del_keyword_collectionlist, get_item_collectionlist
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
-from .klanr import load_module
+from .deou import load_module
 from .tools import create_supergroup
 LOGS = logging.getLogger(" سورس ديو \n ")
 cmdhr = Config.COMMAND_HAND_LER
